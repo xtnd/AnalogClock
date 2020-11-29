c076c24df6fe5131cec3482df367b590f23f8d78
Kody <kodyscharf@hotmail.com>
2020-11-29 01:07:15 -0500

    Changes to make clock hands move, using setIntervals and transform

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+window.onload = function(){
+    const HOURHAND = document.querySelector("#hour");
+    const MINUTEHAND = document.querySelector("#minute");
+    const SECONDHAND = document.querySelector("#second");
+
+    let date = new Date();
+    var hour = date.getHours() - 12
+    var minute = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    let degHour = (360 / 12) * hour;
+    let degMinute = (360 / 30) * minute / 2;
+    let degSecond = (360 / 30) * seconds / 2;
+    let startHour = 360 + (degMinute * 0.5)
+
+    SECONDHAND.style.transform = "rotate(" + degSecond + "deg)"
+    MINUTEHAND.style.transform = "rotate(" + degMinute + "deg)"
+
+    degMinute < 1 
+    ? HOURHAND.style.transform = "rotate(" + startHour + "deg)"
+    : HOURHAND.style.transform = "rotate(" + degHour + "deg)"
+    
+
+         setInterval(function(){
+            degSecond+=6;
+            
+
+            SECONDHAND.style.transform = "rotate(" + degSecond+ "deg)"
+
+            if (degSecond % 360 == 0 ) {
+                degMinute+=6;
+                console.log(degHour)
+                MINUTEHAND.style.transform = "rotate(" + degMinute + "deg)"
+                degHour+=0.5;
+                HOURHAND.style.transform = "rotate(" + degHour + "deg)"
+
+                
+            }
+        },1000)
+
+}
+
+
